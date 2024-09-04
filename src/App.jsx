de54974f14bfccad37c32deb5361b5035edb0085
nyeong <duddnd4457@chosun.ac.kr>
2024-09-04 22:09:30 +0900

    :12.4) 페이지 라우팅 3. 페이지 이동 ~

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-import './App.css'
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Diary from "./pages/Diary";
+import New from "./pages/New";
+import NotFound from "./pages/NotFound";
 
+// TODO:12.4) 페이지 라우팅 3. 페이지 이동
 function App() {
-
   return (
-    <>
-      <h1>Hello, World!</h1>
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/new" element={<New />}></Route>
+      <Route path="/diary" element={<Diary />}></Route>
+      <Route path="*" element={<NotFound />}></Route>{" "}
+    </Routes>
+  );
 }
 
-export default App
+export default App;
